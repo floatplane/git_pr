631c11dd8afa9d0691fce6cdd97b77ef87ba74ee
Brian Sharon <brian@floatplane.us>
2014-10-08 11:30:53 -0700

    Extract run_command and prompt methods

--- lib/git_pr.rb
+++ lib/git_pr.rb
@@ -7,6 +7,36 @@ require "git_pr/diff"
 module GitPr
   # Your code goes here...
 
+  def self.run_command(cmd,
+                       args = {
+                         :failure => lambda {},
+                         :force_print_output => false
+                       })
+    puts cmd.green if $verbose
+    result = `#{cmd}`
+    puts result if $verbose || args[:force_print_output]
+    puts '' if $verbose
+    if $?.exitstatus != 0
+      args[:failure].call()
+      exit -1
+    end
+  end
+
+  def self.get_char
+    state = `stty -g`
+    `stty raw -echo -icanon isig`
+
+    STDIN.getc.chr
+  ensure
+    `stty #{state}`
+    puts ""
+  end
+
+  def self.prompt prompt
+    print prompt
+    return GitPr.get_char.downcase == 'y'
+  end
+
   def self.ensure_remote_for_project git, username, ssh_url, git_url
     # find or add a remote for the PR
     remote = git.remotes.find { |r| [git_url, ssh_url].include? r.url }
